e5f66c8da380f2f3a8181dd50345a5cd4878b358
Tran Son <sontd.hust@gmail.com>
2019-03-28 01:11:27 +0900

    Deploy.

---
+++ precache-manifest.8aae03aab03651c27f5db42cb17edc1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fa96082cf620aa6e86",
+    "url": "/neko-inu/static/css/main.a93604f5.chunk.css"
+  },
+  {
+    "revision": "80fa96082cf620aa6e86",
+    "url": "/neko-inu/static/js/main.a5c0f21f.chunk.js"
+  },
+  {
+    "revision": "779954f8e45719ee0ce4",
+    "url": "/neko-inu/static/js/runtime~main.e15ba7e9.js"
+  },
+  {
+    "revision": "2e33dce479e9ee52756e",
+    "url": "/neko-inu/static/js/2.e81142b6.chunk.js"
+  },
+  {
+    "revision": "6fe6f9621637ddc09e1c9abf3d1fe3fb",
+    "url": "/neko-inu/index.html"
+  }
+];
